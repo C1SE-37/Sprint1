c75a9a9332676cbb94ae5b25e5d2114a83f57969
truonggiahuy7799 <63004049+truonggiahuy7799@users.noreply.github.com>
2021-10-04 13:53:00 +0700

    update xml

---
+++ XML_Xuan/app/src/main/java/com/example/onclinic/LoiChao.java
@@ -0,0 +1,14 @@
+package com.example.onclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoiChao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loichao);
+    }
+}
