0e84e50d2a1544640a5b88e5b95ce0de3900b7ed
truonggiahuy7799 <63004049+truonggiahuy7799@users.noreply.github.com>
2021-10-05 09:40:02 +0700

    final XML

---
+++ app/src/main/java/com/example/onclinic/TrangChuBenhNhan.java
@@ -0,0 +1,20 @@
+package com.example.onclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+public class TrangChuBenhNhan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang_chu_benh_nhan);
+
+
+    }
+}
